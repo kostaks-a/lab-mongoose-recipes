63b27cd568e12ef8d370e63b75f5df989e64fe93
“Konstantinos <kostaks@outlook.com>
2023-02-02 17:29:18 +0100

    missing last iteration

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+mongoose.set('strictQuery', true);
 
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
@@ -17,7 +18,68 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+  
+  /*const greekSalad = {
+    title : "Greek Salad",
+    level : "Easy Peasy",
+    ingredients : " Tomatoes , cucumber , onion , feta , olive oil , salt",
+    cuisine : "Greek",
+    dishType: "other",
+    duration: 10,
+    image : 'https://www.themediterraneandish.com/wp-content/uploads/2020/02/Greek-salad-recipe-3.jpg',
+    creator: "Kostas",
+    created: '02-02-2023',
+  }
+*/
+
+  //const recipe1 = data[0];
+  /*
+
+  Recipe.create(recipe1)
+  .then((rcp) => {
+    console.log("Recipe created", rcp);
   })
+  .catch((err) => {
+    console.log("there was an error", err);
+  });
+*/
+//////// Iteration 3 ///////////
+
+  Recipe.insertMany(data)
+  .then((recipes) => {
+   recipes.forEach(rcp => {
+    console.log('Recipe Name:' ,rcp.title)
+   });
+   ////////////////////////
+   Recipe.findOneAndUpdate(
+    {title: "Rigatoni alla Genovese"},
+    {duration : 100},
+  )
+  .then((rcp) => {
+    console.log ("Duration of recipe changed")
+   })
+   .catch((err) => {
+     console.log("there was an error", err);
+   });
+   //////////////////////
+   
+   Recipe.findOneAndRemove({title: "Carrot Cake"})
+  .then((deleted) => {
+    console.log ("recipe deleted",)
+  })
+  .catch((err) => {
+     console.log("there was an error", err);
+  });
+   
+  })
+  .catch((err) => {
+    console.log("there was an error", err);
+  });
+  
+
+  })
+ 
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
